18fbf482f377af32d45e5b14e6ac0899a0fe1297
Ricardo Jacques <ricardojacques03@gmail.com>
2025-07-01 21:05:13 -0300

    add: Gitignore

--- ECommerce.Api/obj/Debug/net8.0/ECommerce.Api.AssemblyInfo.cs
+++ ECommerce.Api/obj/Debug/net8.0/ECommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1469803f96689717c0a96d5dbdfeea4fb51ddf75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5822ad7517da8dc658cbbb0d47a938b23cf9be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ecommerce2.Client/obj/Debug/net8.0/Ecommerce2.Client.AssemblyInfo.cs
+++ Ecommerce2.Client/obj/Debug/net8.0/Ecommerce2.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce2.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1469803f96689717c0a96d5dbdfeea4fb51ddf75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5822ad7517da8dc658cbbb0d47a938b23cf9be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce2.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce2.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
